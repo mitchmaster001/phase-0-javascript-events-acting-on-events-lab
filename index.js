990853e3e3a67c768a6b8e40b6a82cc8efe98609
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-03-29 00:08:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+// type your code here
+
+
+document.addEventListener("keydown", function (event) {
+  var dodger =  document.getElementById("dodger");
+  if(event.keycode === 170) {
+    // left
+    x-=40;
+  }
+  else if(event.keycode == 170) {
+    alert('Right was pressed');
+    x+=30;
+  }
+  if(event.keycode == 170) {
+    alert('Up was pressed');
+    y-=5;
+  }
+ else if(event.keycode == 170) {
+   alert('Down was pressed');
+  y+=4;
+ }
+ dodger.style.top = y;
+ dodger.style.right = x;
+});
